3855bfd93df29218e817fe676a7f9e11dc519680
lmm3 <lmm3@cin.ufpe.br>
2023-04-11 23:18:46 -0300

    Revert "Refatoracao de Front, remocao de code smells"
    This reverts commit 12ad18c1d75a4ff2ea7e367ce8da325152849e74.

--- web-app/src/pages/Economyseemore/index.tsx
+++ web-app/src/pages/Economyseemore/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import {
   Box,
+  IconButton,
+  useBreakpointValue,
+  Stack,
   Heading,
+  Text,
+  Container,
+  Link,
   SimpleGrid,
 } from '@chakra-ui/react';
-
+// Here we have used react-icons package for the icons
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
+// And react-slick as our Carousel Lib
+import Slider from 'react-slick';
+import { stringify } from 'querystring';
+//import { Link } from 'react-router-dom';
 type Card = {
   title: string;
   image: string
@@ -28,6 +39,14 @@ getEconomy();
 
   
 export default function Economyseemore(){
+    // As we have used custom buttons, we need a reference variable to
+    // change the state
+    const [slider, setSlider] = React.useState<Slider | null>(null);
+  
+    // These are the breakpoints which changes the position of the
+    // buttons as the screen size changes
+    const top = useBreakpointValue({ base: '90%', md: '50%' });
+    const side = useBreakpointValue({ base: '30%', md: '40px' });
     
   return(
     <Heading>
